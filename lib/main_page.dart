0ed9bde55f73204eb65f07e38efecb86008a6cf8
beuzasen <ebeyzanurs@gmail.com>
2024-08-05 15:32:50 +0300

    initial commit

---
+++ lib/main_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final TextEditingController _textController = TextEditingController(text: '');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Qr code generator'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          const SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: TextField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(10),
+                  labelText: 'Enter Text',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromARGB(255, 0, 146, 20), width: 2.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 2.0),
+                  )),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
